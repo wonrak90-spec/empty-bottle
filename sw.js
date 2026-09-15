bf218ee3616666ddae51cc9997a09beabbcb52c1
wonrak90-spec <wonrak90@gmail.com>
2026-09-15 20:44:48 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = '공병입고-v15';
+const CACHE = '공병입고-v16';
 const SHELL = ['./', './index.html', './app.js', './config.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
